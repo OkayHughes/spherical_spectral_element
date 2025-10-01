cca87149c73a0fdb6ccd56ce5457ffbccb3cf194
Owen Hughes <okayhughes@gmail.com>
2025-10-01 11:53:00 -0400

    Refactor spectral tests to use module-level definition of npt.

--- tests/test_spectral.py
+++ tests/test_spectral.py
@@ -5,10 +5,10 @@ from spherical_spectral_element.spectral import Deriv
 
 def test_quadrature():
   npt = 4
-  deriv = Deriv(npt)
+  deriv = Deriv()
   assert(np.allclose(np.sum(deriv.gll_weights * deriv.gll_points**2), 2/3 ))
 
 def test_generate_derivative():
   npt = 4
-  deriv = Deriv(npt)
+  deriv = Deriv()
   assert(np.allclose(np.dot(deriv.deriv, deriv.gll_points**2 - deriv.gll_points**3),  (2*deriv.gll_points - 3 * deriv.gll_points**2)))
