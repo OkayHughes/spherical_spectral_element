cb6b40e8108b03860662bb90e89b9162226fe506
Owen Hughes <okayhughes@gmail.com>
2025-09-30 23:15:20 -0400

    Refactor spectral so deriv is an object. Update tests accordingly. All tests pasing

--- spherical_spectral_element/spectral.py
+++ spherical_spectral_element/spectral.py
@@ -1,25 +1,35 @@
 from .config import np
 
-# we assume this is fixed for the moment.
-p_points = np.array([1.0, np.sqrt(1/5), -np.sqrt(1/5), -1.0])
-g_weights = np.array([1/6, 5/6, 5/6, 1/6])
-npt = len(p_points)
+class Deriv:
+  """ """
+  def __init__(self, npt):
+    self.npt = npt
+    self.init_quadrature()
+    self.init_deriv()
+
+  def init_quadrature(self):
+    if self.npt == 4:
+      # we assume this is fixed for the moment.
+      self.gll_points = np.array([1.0, np.sqrt(1/5), -np.sqrt(1/5), -1.0])
+      self.gll_weights = np.array([1/6, 5/6, 5/6, 1/6])
+    else:
+      raise ValueError(f"GLL points not computed for npt={self.npt}")
+  def init_deriv(self):
+    # uses the lagrange interpolating polynomials
+    leg_eval = np.zeros(shape=(self.npt, self.npt))
+    leg_der = np.zeros(shape=(self.npt, self.npt))
+
+    for deg in range(self.npt):
+      c = np.zeros(self.npt)
+      c[deg] = 1.0
+      leg_eval[:, deg] = np.polynomial.legendre.legval(self.gll_points, c)
+      der = np.polynomial.legendre.legder(c, 1)
+      leg_der[:, deg] = np.polynomial.legendre.legval(self.gll_points, der)
+
+    coeffs = np.linalg.inv(leg_eval)
+    self.deriv = np.dot(leg_der, coeffs)
 
 
-def generate_derivative():
-  # uses the lagrange interpolating polynomials
-  leg_eval = np.zeros(shape=(npt, npt))
-  leg_der = np.zeros(shape=(npt, npt))
-
-  for deg in range(npt):
-    c = np.zeros(npt)
-    c[deg] = 1.0
-    leg_eval[:, deg] = np.polynomial.legendre.legval(p_points, c)
-    der = np.polynomial.legendre.legder(c, 1)
-    leg_der[:, deg] = np.polynomial.legendre.legval(p_points, der)
-
-  coeffs = np.linalg.inv(leg_eval)
-  deriv = np.dot(leg_der, coeffs)
 
 
-  return deriv
+
