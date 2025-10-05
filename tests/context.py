bbe3543b8e75e31cdae825384275d86ca511e4e0
Owen Hughes <okayhughes@gmail.com>
2025-10-05 18:35:19 -0400

    Start creating central way to test for numerical closeness.

--- tests/context.py
+++ tests/context.py
@@ -2,6 +2,18 @@
 
 import sys
 import os
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import spherical_spectral_element
+from spherical_spectral_element.config import np
+
+
+def compare_fp_close_nonzero(a, b):
+  pass
+
+def compare_num_close(a, b, dx):
+  pass
+
+def compare_to_zero(a):
+  pass
