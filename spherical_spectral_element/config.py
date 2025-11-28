8148f5168fa7a980248cc2ec765a7a79fd3cb490
Owen Hughes <okayhughes@gmail.com>
2025-11-28 18:41:36 -0500

    Give module level definition of partial

--- spherical_spectral_element/config.py
+++ spherical_spectral_element/config.py
@@ -27,9 +27,11 @@ if use_jax:
   def jax_unwrapper(x):
     return np.asarray(x)
   jit = jax.jit
-  
+
   def versatile_assert(should_be_true):
     return
+
+  from jax.tree_util import Partial as partial
 else:
   import numpy as jnp
 
@@ -41,6 +43,8 @@ else:
 
   def jit(func, *_, **__):
     return func
-  
+
   def versatile_assert(should_be_true):
     assert should_be_true
+
+  from functools import partial
